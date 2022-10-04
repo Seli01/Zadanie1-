6111e8ab10d2fb33f13f524cd623dab447ba5662
Seli01 <114980879+Seli01@users.noreply.github.com>
2022-10-04 19:34:07 +0200

    Update Address.cs

--- Models/Address.cs
+++ Models/Address.cs
@@ -1,17 +1,33 @@
-﻿using System;
+using EmployeeRegister_K63_2022.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmployeeRegister_K63_2022.Models
 {
+    [Serializable]
     public class Address
     {
+        
+       
+
         public int Id { get; set; }
+
+        [ModelPropertyLabel(Label = "City", Priority = 5)]
         public string City { get; set; }
+
+        [ModelPropertyLabel(Label = "PostalCode", Priority = 6)]
         public string PostalCode { get; set; }
-        public string Street { get; set; }
-        public string HouseNumber { get; set; }
+
+        [ModelPropertyLabel(Label = "Street", Priority = 7)]
+        public int Street { get; set; }
+
+        [ModelPropertyLabel(Label = "HouseNumber", Priority = 8)]
+        public char HouseNumber { get; set; }
+
     }
 }
+
